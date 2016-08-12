ab9cc31ea6cc75726bfc6439fd117caf9fe3778f
Joey Gordon <joey@theblackaxe.com>
2016-08-12 14:48:16 -0700

    updates the bio area some. adds the type effect on the header stuff

--- app/js/app.js
+++ app/js/app.js
@@ -7,35 +7,44 @@ $(function() {
 
 
   // header scrolling magic
-  $(window).scroll(function(){
-	  if($(this).scrollTop() > 1) {
-	  	var wScroll = $(this).scrollTop();
+  function scrollIt() {
+	  $(window).scroll(function(){
+		  if($(this).scrollTop() > 1) {
+		  	var wScroll = $(this).scrollTop();
 
 
-	  	var headTop = $('header').offset().top;
-	  	var offset = Math.max(0, wScroll/2 - 20);
-	  	var offset2 = Math.max(0, wScroll/3 - 20)
+		  	var headTop = $('header').offset().top;
+		  	var offset = Math.max(0, wScroll/2 - 20);
+		  	var offset2 = Math.max(0, wScroll/3 - 20)
 
-	  	$('.shadow').addClass('scrolled');
+		  	$('.shadow').addClass('scrolled');
 
-	  	if (wScroll > headTop) {
-		  	$('.info').css({'transform': 'translate(0px, -'+ offset +'px)'});
-		  	$('.shadow').css({'transform': 'translate(0px, -'+ offset2 +'px)'});
-	  	}
+		  	if (wScroll > headTop) {
+			  	$('.info').css({'transform': 'translate(0px, -'+ offset +'px)'});
+			  	$('.shadow').css({'transform': 'translate(0px, -'+ offset2 +'px)'});
+		  	}
 
-	  	if (wScroll > headTop + 50) {
-	  		$('.shadow').addClass('scrolled');
-	  		$('.chill').addClass('scrolled');
-	  	}else{
-	  		$('.shadow').removeClass('scrolled');
-	  		$('.chill').removeClass('scrolled');
-	  	}
-	  }
+		  	if (wScroll > headTop + 50) {
+		  		$('.shadow').addClass('scrolled');
+		  		$('.chill').addClass('scrolled');
+		  	}else{
+		  		$('.shadow').removeClass('scrolled');
+		  		$('.chill').removeClass('scrolled');
+		  	}
+		  }
+	  });
+}
+
+
+
+  // type out trigger
+  $(".info span").typed({
+      strings: ["Hi, my name is Joey Gordon. I like to build things for <strong>iOS</strong> and the <strong>web</strong>. I wear a lot of black, eat a lot of <strong>cookies</strong>, and enjoy Starbucks more than I probably should.*"],
+      typeSpeed: 0,
+      callback: function() {
+        scrollIt()
+      }
   });
-
-
-
-  // tooltip stuff
   
 
 });
